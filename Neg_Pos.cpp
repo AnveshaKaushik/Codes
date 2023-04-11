9f6ff21beae7d2b71666af7e7f70bed87b3d6e79
Anvesha Kaushik <anvesha.tyagi261@gmail.com>
2023-04-12 00:17:25 +0530

    Code 5
    Move all negative numbers to beginning and positive to end

---
+++ Neg_Pos.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {-12,11,13,15,-5,-7,6,5,-3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr2[n];
+
+    int countN = 0;
+    int countP = n-1;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] < 0)
+        {
+            arr2[countN] = arr[i];
+            countN++;
+        }
+        else
+        {
+            arr2[countP] = arr[i];
+            countP--;
+        }
+    }
+
+    for(int j = 0; j < n; j++)
+    {
+        arr[j] = arr2[j];
+    }
+
+    for(int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    return 0;
+}
